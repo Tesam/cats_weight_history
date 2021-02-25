7cce60573c46c4d53913f0a15fdc84b60e69a2e0
Tesam <mtsamudiog@gmail.com>
2021-02-25 18:34:33 -0300

    feat(home.dart): add the home drawer with the header and items

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -8,6 +8,49 @@ class Home extends StatelessWidget {
         title: Text('Cats'),
         centerTitle: true,
       ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              child: Text(
+                'Cats Weight History',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.green,
+              ),
+            ),
+            ListTile(
+              title: Text(
+                'Cats',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text(
+                'Weight History',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text(
+                'Breed',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
