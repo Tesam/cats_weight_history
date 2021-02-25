1407337a0685f0be9042f45cc16df8d5995089b0
Tesam <mtsamudiog@gmail.com>
2021-02-25 18:44:06 -0300

    feat(weight_history.dart): add the weight_history.dart file with a simple app bar

---
+++ lib/weight_history/weight_history.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class WeightHistory extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Weight History'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
