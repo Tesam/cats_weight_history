68027dfee87037a692c46d6a4928af0ef44e1b1b
Tesam <mtsamudiog@gmail.com>
2021-02-27 09:53:13 -0300

    feat(breed.dart): add breed.dart screen with a simple scaffold

---
+++ lib/breed/breed.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Breed extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Breeds'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
