f4dc50faec7d0dc3f40d6dffa0123a1be0f6d9aa
Tesam <mtsamudiog@gmail.com>
2021-02-27 16:23:25 -0300

    feat(breed_bloc.dart): add the getAllBreeds function using streams

--- lib/breed/bloc/breed_bloc.dart
+++ lib/breed/bloc/breed_bloc.dart
@@ -1,10 +1,28 @@
+import 'dart:async';
+
 import 'package:cats_weight_history/breed/model/breed.dart';
 import 'package:cats_weight_history/breed/repository/breed_repository.dart';
 
 class BreedBloc {
   final _breedRepository = BreedRepository();
 
+  final _breedController = StreamController<List<Breed>>.broadcast();
+  get breeds => _breedController.stream;
+
+  BreedBloc(){
+    getAllBreeds();
+  }
+
   insertBreed(Breed breed) async {
     await _breedRepository.insertBreed(breed);
   }
+
+  getAllBreeds() async{
+    _breedController.sink.add(await _breedRepository.getAllBreeds());
+  }
+
+  dispose() {
+    _breedController.close();
+  }
+
 }
