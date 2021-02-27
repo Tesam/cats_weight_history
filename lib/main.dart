e81014b76b7a019fd9a03ba62c4c482629026459
Tesam <mtsamudiog@gmail.com>
2021-02-27 10:11:39 -0300

    feat(main.dart): add a home class to manage the nav drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:cats_weight_history/breed/breed.dart';
 import 'package:cats_weight_history/cat/cat.dart';
+import 'package:cats_weight_history/weight_history/weight_history.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,8 +17,88 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Cat(),
+      home: Home(),
     );
   }
 }
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Widget currentBody = Cat();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cats'),
+        centerTitle: true,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              child: Text(
+                'Cats Weight History',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.green,
+              ),
+            ),
+            ListTile(
+              title: Text(
+                'Cats',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              onTap: () {
+                setState(() {
+                  Navigator.of(context).pop();
+                  currentBody = Cat();
+                });
+              },
+            ),
+            ListTile(
+              title: Text(
+                'Weight History',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              onTap: () {
+                setState(() {
+                  Navigator.of(context).pop();
+                  currentBody = WeightHistory();
+                });
+              },
+            ),
+            ListTile(
+              title: Text(
+                'Breed',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              onTap: () {
+                Navigator.of(context).pop();
+                currentBody = Breed();
+              },
+            ),
+          ],
+        ),
+      ),
+      body: currentBody,
+    );
+  }
+}
+
 
