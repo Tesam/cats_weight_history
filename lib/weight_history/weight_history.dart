98532774e912da82573c3fec21fbf287cc6a124d
Tesam <mtsamudiog@gmail.com>
2021-02-27 10:15:25 -0300

    refactor(weight_history.dart): delete the unused scaffold and add a simple container with a text widget

--- lib/weight_history/weight_history.dart
+++ lib/weight_history/weight_history.dart
@@ -3,11 +3,8 @@ import 'package:flutter/material.dart';
 class WeightHistory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Weight History'),
-        centerTitle: true,
-      ),
+    return Container(
+      child: Text('Weight History'),
     );
   }
 }
