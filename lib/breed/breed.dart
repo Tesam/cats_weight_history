9295a2b95333c1ecb05f29ee32103d46c5dd12a8
Tesam <mtsamudiog@gmail.com>
2021-02-27 10:52:27 -0300

    feat(breed.dart): add a static breed list into cards

--- lib/breed/breed.dart
+++ lib/breed/breed.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
 class Breed extends StatelessWidget {
+  final items = ['Americano de Pelo Duro', 'Asiático', 'Azul Ruso', 'Bombay', 'Bengalí'];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Breed'),
+    return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return Card( //                           <-- Card
+          child: ListTile(
+            title: Text(items[index]),
+            trailing: Icon(
+              Icons.delete,
+              color: Colors.red.shade400,
+            ),
+          ),
+        );
+      },
     );
   }
 }
