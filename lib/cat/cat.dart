14799441cc407235102a1a5a16f525b848bb8372
Tesam <mtsamudiog@gmail.com>
2021-02-27 10:14:04 -0300

    refactor(cat.dart): delete the unused scaffold and add a simple container with a text widget

--- lib/cat/cat.dart
+++ lib/cat/cat.dart
@@ -1,68 +1,10 @@
-import 'package:cats_weight_history/breed/breed.dart';
-import 'package:cats_weight_history/weight_history/weight_history.dart';
 import 'package:flutter/material.dart';
 
 class Cat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Cats'),
-        centerTitle: true,
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              child: Text(
-                'Cats Weight History',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                ),
-              ),
-              decoration: BoxDecoration(
-                color: Colors.green,
-              ),
-            ),
-            ListTile(
-              title: Text(
-                'Cats',
-                style: TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-            ),
-            ListTile(
-              title: Text(
-                'Weight History',
-                style: TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-              onTap: () {
-                Navigator.of(context).pop();
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (BuildContext context) => WeightHistory()));
-              },
-            ),
-            ListTile(
-              title: Text(
-                'Breed',
-                style: TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-              onTap: () {
-                Navigator.of(context).pop();
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (BuildContext context) => Breed()));
-              },
-            ),
-          ],
-        ),
-      ),
+    return Container(
+      child: Text('Cats'),
     );
   }
 }
