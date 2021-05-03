da03d44c7144dafdf11b70e7b24fade628f6673e
Tesam <mtsamudiog@gmail.com>
2021-05-02 21:47:36 -0400

    feat(cat.dart): add cat model

---
+++ lib/cat/model/cat.dart
@@ -0,0 +1,15 @@
+class Cat{
+  final int id;
+  final int breedId;
+  final String cat;
+
+  Cat({this.id, this.breedId, this.cat});
+
+  Map<String, dynamic> toMap() {
+    return {
+      "id": id,
+      "breed_id": breedId,
+      "cat": cat,
+    };
+  }
+}
