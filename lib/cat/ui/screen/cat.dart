c700dc84b3b8d69ab411aba12e9661fd04c5a001
Tesam <mtsamudiog@gmail.com>
2021-05-02 21:44:22 -0400

    refactor(cat.dart): make a stateful screen

--- lib/cat/ui/screen/cat.dart
+++ lib/cat/ui/screen/cat.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 
-class Cat extends StatelessWidget {
+class Cat extends StatefulWidget {
+  @override
+  _CatState createState() => _CatState();
+}
+
+class _CatState extends State<Cat> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Cats'),
+    return Scaffold(
+      body: Center(
+        child: Text("Cat"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon( Icons.add, ),
+          onPressed: () => {},
+      )
     );
   }
 }
